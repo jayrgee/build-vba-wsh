72f74906ca6a2ca5e28418eee7b17700707c8373
jayrgee <jayrgee@fastmail.fm>
2017-02-28 13:20:16 +1100

    wip DoJSON.js

--- examples/DoJSON.js
+++ examples/DoJSON.js
@@ -1,25 +1,24 @@
-includeFile("..\\lib\\json2.js");
-
-if (!this.JSON) WScript.Echo("JSON DOESN'T EXIST");
-
 var fso = new ActiveXObject("Scripting.FileSystemObject");
-var pathFile = "config.json";
-
-WScript.Echo(pathFile);
-var fileStream = fso.openTextFile(pathFile);
-var fileData = fileStream.readAll();
-fileStream.Close();
-
-var config = JSON.parse(fileData);
-WScript.Echo(JSON.stringify(config));
+includeJS("..\\lib\\json2.js");
 
 
+(function (JSON, fso) {
+  var pathFile = "config.json";
+
+  WScript.Echo(pathFile);
+  var fileStream = fso.openTextFile(pathFile);
+  var fileData = fileStream.readAll();
+  fileStream.Close();
+
+  var config = JSON.parse(fileData);
+  WScript.Echo(JSON.stringify(config));
+
+}(this.JSON, fso));
 
 
-function includeFile(filename) {
-    var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var fileStream = fso.openTextFile(filename);
-    var fileData = fileStream.readAll();
-    fileStream.Close();
-    eval(fileData);
+function includeJS(filename) {
+  var fileStream = fso.openTextFile(filename);
+  var fileData = fileStream.readAll();
+  fileStream.Close();
+  eval(fileData);
 }
