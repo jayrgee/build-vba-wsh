1c8e66974119860618ac4441164f5b1e8e0a75b9
jayrgee <jayrgee@fastmail.fm>
2017-02-28 23:42:46 +1100

    create-dotm wip

--- examples/create-dotm.js
+++ examples/create-dotm.js
@@ -10,7 +10,7 @@ function includeJS(filename) {
   var f = fso.GetFile(pathScript);
 
   filename = fso.GetParentFolderName(f) + "\\" + filename + ".js";
-  WScript.Echo(filename);
+  //WScript.Echo(filename);
 
   var fileStream = fso.openTextFile(filename);
   var fileData = fileStream.readAll();
@@ -24,11 +24,23 @@ function includeJS(filename) {
 (function (config) {
 
   config = config || {};
-  WScript.Echo(JSON.stringify(config));
+  //WScript.Echo(JSON.stringify(config));
 
   var wdFormatXMLTemplateMacroEnabled = 15;
 
+  var appWd = new ActiveXObject("Word.Application");
+  appWd.Visible = true;
+  var doc = appWd.Documents.Add();
 
+  var refs = [];
+  if (config.VBProject.References) {
+    refs = config.VBProject.References;
+  }
+  var i = 0;
+  for (i = 0; i < refs.length; i++) {
+    WScript.Echo(refs[i]);
+    doc.VBProject.References.AddFromFile(refs[i]);
+  }
 
 
 
